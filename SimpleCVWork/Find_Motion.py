6e5df1e1dce18ce3d5a840ae5ed00fd10af8a965
Matt Bellis <matthew.bellis@gmail.com>
2014-07-03 15:53:09 -0400

    Some changes during debugging

--- SimpleCVWork/Find_Motion.py
+++ SimpleCVWork/Find_Motion.py
@@ -10,18 +10,24 @@ Created on Thu Jun 19 11:55:10 2014
 
 from SimpleCV import Camera, Color, Display
 
-cam= Camera(1)
+cam= Camera(0)
 previous=cam.getImage()
-
 
 disp= Display(previous.size())
 
 while not disp.isDone():
     current=cam.getImage()
-    motion=current.findMotion(previous)
+    #motion=current.findMotion(previous,method='BM') # Robust but slow.
+    #motion=current.findMotion(previous,method='LK')
+    # We should learn about the ``window" kwarg.
+    motion=current.findMotion(previous,method='HS',window=11)
+    #motion=current.findMotion(previous,method='HS',window=4)
+    #motion.draw()
+    #print motion
     for m in motion:
         m.draw(color=Color.RED,normalize=False)
         
     current.save(disp)
     
+    #current.show()
     previous=current 
